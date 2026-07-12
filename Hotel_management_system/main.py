19cdaa9c263a65d2d828cf7e0ab4851e9b19fdd0
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-07-12 19:52:01 +0530

    Working on

--- Hotel_management_system/main.py
+++ Hotel_management_system/main.py
@@ -6,42 +6,28 @@ print("Welcome to PY HOTELS")
 print("***********************")
 
 print("Manager - How may i help you sir?\n1- Want a room\n2- Dine at restaurant\n3- Exit")
+class hotelroom():
+    def room(self , n):
+        self.n = n
+        print("Manager- Will you be staying overnight, or is this for a One day?\n1- One day\n2- One night\n3- Few hours only")
+    def shift(self , n):
+        self.n = n
+        print("**Here our list**")
+        for room_no, room_data in mid_range_rooms.items(): #actually we are in nested dict so we have to do this try of logic for "for loop"
+            print(f"Room no-{room_no} which is {room_data['category']} and its rent is {room_data['rent_per_day']}/-")
+        print("Manager: select which rooms you want.")
 
-def book():
-    pass
-def dine():
-    pass
+    def select(self , n):
+        self.n = n
+        room_data = mid_range_rooms[q]
+        print(f"Thanks sir for booking room no {q}! Here is your bill:\nRoom type: {room_data['category']}\nRoom rent: {room_data['rent_per_day']}")
+        
 
-while True:
-    user= int(input("Choose your option from above: "))
-    while True:
-        if user == 1:
-            print("Manager: Most welcome sir! We have Three Types of rooms Available\n1- Mid range rooms\n2- Semi_luxury_rooms\n3- premium_rooms \n4- Exit\nWhich Type of room you want sir?")
-            while True:
-                a = int(input("Choose room type: "))
-                if a == 1:
-                    print("Manager- Will you be staying overnight, or is this for a One day?\n1- One day\n2- One night\n3- Few hours only")
-                    while True:
-                        b = int(input("Enter your opinion: "))
-                        if b == 1:
-                            print("**Here our list**")
-                            for room_no, room_data in mid_range_rooms.items(): #actually we are in nested dict so we have to do this try of logic for "for loop"
-                                print(f"Room no-{room_no} which is {room_data['category']} and its rent is {room_data['rent_per_day']}/-")
-                        print("Manager: select which rooms you want.")
-                        q = int(input("select rooms: "))
-                        room_data = mid_range_rooms[q]
-                        print(f"Thanks sir for booking room no {q}! Here is your bill:\nRoom type: {room_data['category']}\nRoom rent: {room_data['rent_per_day']}")
-                        
-                        exit()
-        elif user == 2:
-            dine()
-            exit()
-        elif user == 3:
-            print("Thanks")
-            exit()
-        else:
-            print("Choose between 1to3 only")
-            break
+a = hotelroom()
+x = int(input("Choose your option from above: "))
 print("")
-print("")
-print("")
+a.room(x)
+b = int(input("Enter your opinion: "))
+a.shift(b)
+q = int(input("select room: "))
+a.select(q)
