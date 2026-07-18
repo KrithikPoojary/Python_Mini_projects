9e7326a9a994c15b5b6e773721699eecaab8decf
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-07-18 23:16:53 +0530

    Added 2nd function

--- Feature_extraction/main.py
+++ Feature_extraction/main.py
@@ -21,4 +21,34 @@ def extract(title):
         if i == "(":
             see = True
 
+a['Episodes'] = a["Title"].apply(extract)   # so this will mainly apply the changes and insert the new column to the file.
 
+a['Episodes'] = a['Episodes'].str.replace(" eps" , "")  #Here i have replaced because it is str and in future i have to do agg.func which only integrate with int 
+print(a)
+
+type(a.loc[0]['Episodes'])     #Just for surety we have seen that it is string
+
+a['Episodes'] = a['Episodes'].astype(int)  # through this we had change the type 'str' to 'int'
+
+type(a.loc[0]['Episodes'])    #just to recheck 
+
+
+
+#Just to analyse the changes
+a.loc[2]['Title']   # just checking 
+a.loc[34]['Title']   # just checking 
+a.loc[8]['Title']   # just checking 
+a.loc[1]['Title']   # just checking 
+
+
+#ADDED next functions
+
+def extract2(date):
+    check = False
+    time = ""
+    for i in range(len(date)):
+        if date[i] == ")":
+            for j in range(i+1 , i+20):
+                time += date[j]
+            return time
+                
