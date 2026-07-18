138444878a5cdc1fa3b7a9f24803db7ee8f80616
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-07-18 23:12:38 +0530

    First function

--- Feature_extraction/main.py
+++ Feature_extraction/main.py
@@ -3,3 +3,22 @@ import pandas as pd
 
 a = pd.read_csv(r'Feature_extraction/anime.csv')
 print(a)
+
+a.head()   # This will give us top rated files
+a.loc[4]['Title']   #Through this we can analysis the contant
+
+# Here the first function to extract more information
+
+def extract(title):
+    see = False
+    episode = ""
+    for i in title:
+        if i == ")":
+            see = False
+            return episode
+        if see == True:
+            episode+=i
+        if i == "(":
+            see = True
+
+
